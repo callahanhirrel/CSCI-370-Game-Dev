44447ebd5a5c3781dac24eb132429ab5d8c70ae8
callahanhirrel <callahan.hirrel@gmail.com>
2019-01-29 09:27:29 -0600

    First simple game from class
    roll a ball. beat the time. don't hit the green things.

---
+++ CameraTemplate/Assets/Scripts/Controller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private float speed;
+    private bool start;
+    private int points;
+    public TextMesh textPoints;
+    public TextMesh textTime;
+    public float timeRemaining = 15f;
+
+    void GameOver()
+    {
+        start = false;
+        transform.position = new Vector3(-0.2f, 0.82f, -51.79f);
+    }
+
+    // Use this for initialization
+    void Start () {
+        speed = 8;
+        start = false;
+        points = 70;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            start = true;
+        }
+
+        if (start)
+        {
+            timeRemaining -= Time.deltaTime;
+            textTime.text = "Time: " + timeRemaining.ToString();
+
+            if (timeRemaining > 0)
+            {
+                transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f,
+                            Input.GetAxis("Vertical") * Time.deltaTime * speed);
+                // Input.GetAxis directly mapped to arrow keys and WASD
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "cubes")
+        {
+            points -= 10;
+            Destroy(collision.gameObject);
+            textPoints.text = "Points: " + points.ToString();
+            if (points <= 0)
+            {
+                GameOver();
+            }
+        } else if (collision.collider.name == "Wall3")
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+
+    }
+}
---
+++ CameraTemplate/Assets/Scripts/HUDCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDCamera : MonoBehaviour {
+
+    public Transform transformObject;
+    private Vector3 distance;
+    private float smooth;
+
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - transformObject.position;
+        smooth = 0.5f;
+    }
+	
+	// LateUpdate is called once per frame calls itself AFTER regular Update()
+	void LateUpdate () {
+        Vector3 newPosition = transformObject.position + distance;
+        transform.position = Vector3.Slerp(transform.position, newPosition, smooth); 
+        // Slerp gives us smooth movement, instead of hard jumpiness
+	}
+}
