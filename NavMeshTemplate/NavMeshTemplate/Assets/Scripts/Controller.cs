3d3e2599f95dd8f928cec03b7f6a291463188fb2
callahanhirrel <callahan.hirrel@gmail.com>
2019-02-26 09:14:14 -0600

    AI navigation with NavMesh

---
+++ NavMeshTemplate/NavMeshTemplate/Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // necessary to import this when using NavMeshAgent
+
+public class Controller : MonoBehaviour
+{
+    public NavMeshAgent meshAgent;
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 indicates a left click
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition); // so this is a Camera method for a direct ray
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                meshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
